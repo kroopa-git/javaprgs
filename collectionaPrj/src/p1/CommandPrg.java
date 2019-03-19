e6acc64ecf35c46f9c31728ed4aeaea303f58002
lenovo <lenovo@172.23.172.200>
2019-03-19 14:46:18 +0530

    First Commit

---
+++ collectionaPrj/src/p1/CommandPrg.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class CommandPrg {
+
+	public static void main(String[] args) {
+		if(args.length==2)
+		{
+			System.out.println(args[0]);
+			System.out.println(args[1]);
+		}
+		else
+		{
+			System.out.println("Invalid Number of Arguments");
+		
+		}
+
+	}
+
+}
